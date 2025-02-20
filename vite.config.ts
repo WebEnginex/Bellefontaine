374269fe81ff94c08ee5c981bdb50730a25e5312
ArnaudTraisnel59 <bellefontainearnaud@gmail.com>
2025-02-20 21:22:26 +0100

    fix: configuration vite pour la production

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
-    strictPort: true,
-  },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    outDir: "dist",
-    sourcemap: true,
-    manifest: true,
+  preview: {
+    host: true, // Nécessaire pour écouter sur toutes les interfaces
+    port: Number(process.env.PORT) || 3000,
   },
-});
+  server: {
+    host: true, // Nécessaire pour écouter sur toutes les interfaces
+    port: Number(process.env.PORT) || 3000,
+  }
+})
